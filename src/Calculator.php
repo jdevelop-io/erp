6b7b4262d7c8fae63afdf315bb7e5cb8bb5a5e92
Jean-Denis VIDOT <contact@jdevelop.io>
2024-10-21 13:55:01 +0200

    :tada: chore!: initial commit using github repository template jdevelop-io/template-php

---
+++ src/Calculator.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JDevelop\Erp;
+
+final class Calculator
+{
+    public function add(int $int, int $int1): int
+    {
+        return $int + $int1;
+    }
+}
