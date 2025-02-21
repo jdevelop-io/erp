21ac83a30df9ba5b1b57bd2a14c14183131d6fde
Jean-Denis VIDOT <contact@jdevelop.io>
2025-02-21 20:33:06 +0100

    :card_file_box: chore(Company): Add a migration to create company table

---
+++ migrations/Version20250221192939.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250221192939 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create company table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $table = $schema->createTable('company_company');
+        $table->addColumn('registration_number', 'string', ['length' => 9]);
+        $table->addColumn('name', 'string', ['length' => 255]);
+        $table->setPrimaryKey(['registration_number']);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $schema->dropTable('company_company');
+    }
+}
